6ad74d815268c369b9b691205f00780f73946309
Aseka <grooves97@gmail.com>
2019-03-27 20:44:59 +0600

    Игра с картами начало дженериковчанских

---
+++ GenericHW/GenericHW/Card.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericHW
+{
+    public enum CardTypes
+    {
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King,
+        Ace
+    }
+
+    public enum CardSuits
+    {
+        Diamonds,
+        Hearts,
+        Spades,
+        Clubs
+    }
+
+    public class Card
+    {
+        public CardTypes CardTypes { get; set; }
+        public CardSuits CardSuits { get; set; }
+    }
+}
